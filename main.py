fed8643693ac48deb89572fce2649e9eca7bef86
marouen-lamiri <marouen.lamiri@gmail.com>
2026-03-20 15:39:14 -0400

    Update main.py
    Add missing variable

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import re
 
 # --- CONFIGURATION ---
 SEARCH_QUERY = "Unity Developer|Unreal Developer|Game Developer|AR Developer|VR Developer|XR Developer|AR/VR Developer|Mobile Developer|Unity Engineer|Unreal Engineer|Game Engineer|AR Engineer|VR Engineer|XR Engineer|AR/VR Engineer|Mobile Engineer"
+SEARCH_QUERY_JOBSPY = f"({SEARCH_QUERY_STR.replace('|', ' OR ')})"
+
 SKILL_WEIGHTS = {
     "Unity": 5, "C#": 5, 
     
